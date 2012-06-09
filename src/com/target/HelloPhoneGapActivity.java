a5a740acf5f8b5efb6d1916ccc753bf8f343c240
yeshbourne <yeshwanth.gowda@target.com>
2012-06-09 18:00:57 +0530

    shoot and

---
+++ src/com/target/HelloPhoneGapActivity.java
@@ -0,0 +1,13 @@
+package com.target;
+
+import android.os.Bundle;
+import org.apache.cordova.*;;
+
+public class HelloPhoneGapActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
